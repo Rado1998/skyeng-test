22caf2237ab05760d18f50e90fe145c272c54f4d
Rado1998 <rado.poghossian98@gmail.com>
2020-01-07 20:54:50 +0400

    changed import

--- src/app/views/main/main.module.ts
+++ src/app/views/main/main.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { MainRoutingModule } from './main-routing.module';
 import { MainView } from './main.view';
 import { CommonModule } from '@angular/common';
-import { TestComponent } from 'src/app/core/components';
+import { TestComponent } from '../../core/components';
 import { TabsModule } from '../../shared/modules/tabs/tabs.module';
 import { IfViewPortSizeModule } from '../../shared/modules/if-view-port-size/if-view-port-size.module';
 
